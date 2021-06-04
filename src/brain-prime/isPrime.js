bcc25a902ad50e422f5301f1a71c4bceafb31091
Xazret <XazikAger@yandex.ru>
2021-06-05 00:07:47 +0300

    fix iteration

--- src/brain-prime/isPrime.js
+++ src/brain-prime/isPrime.js
@@ -2,7 +2,7 @@ const isPrime = (number) => {
   if (number === 1) {
     return 'no';
   }
-  for (let i = 2; i * i <= number; i + 1) {
+  for (let i = 2; i * i <= number; i += 1) {
     if (number % i === 0) {
       return 'no';
     }
